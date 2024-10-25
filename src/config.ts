827ff59f37a1d4b05bcc6adb8ccf80ac9b8f0d6f
yohn-maistre <josejr2498@gmail.com>
2024-10-25 11:38:47 +0900

    added post

--- src/config.ts
+++ src/config.ts
@@ -1,25 +1,25 @@
-export const SITE_FAVICON = "/favicon.png";
-export const SITE_LOGO = "/journal-logo.svg";
-export const SITE_TITLE = "Journal";
+export const SITE_FAVICON = "../favicon.ico";
+export const SITE_LOGO = "/journal/journal-logo.svg";
+export const SITE_TITLE = "yose's journal in the cloud";
 export const SITE_DESCRIPTION =
-  "Blog";
+  "yose's journal in the cloud";
 
-export const MENUS = [
+  export const MENUS = [
   {
-    title: "Posts",
-    link: "/posts",
+    title: "yose.is-a.dev",
+    link: "https://yose.is-a.dev",
     target: "_self",
   },
-  {
-    title: "Tags",
-    link: "/tags",
-    target: "_self",
-  },
-  {
-    title: "Download",
-    link: "https://new-ui.com/templates/journal",
-    target: "_blank",
-  }
+  // {
+  //   title: "Tags",
+  //   link: "/journal/tags/",
+  //   target: "_self",
+  // },
+  // {
+  //   title: "Download",
+  //   link: "https://new-ui.com/templates/journal",
+  //   target: "_blank",
+  // }
 ];
 
-export const FOOTER_CONTENT = "Love Everyone";
+export const FOOTER_CONTENT = "CAUTE";
